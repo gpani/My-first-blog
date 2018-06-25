b8fac99a0df80655f49de09842f7ac48f9d8cbf3
gessi <gessi.paniagua@gmail.com>
2018-06-25 16:06:05 -0300

    Update django

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,16 +39,15 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles','blog',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'mysite.urls'
 
@@ -79,9 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangogirls',
         'USER': 'gessi',
-        'PASSWORD': '',
+        'PASSWORD': 'gessi',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': ''
     }
 }
 
